ba7f526fe697eb55a9d2dee4f810f0983ab56e39
Tigran <tigra2820@mail.ru>
2020-12-12 08:56:43 +0300

    init commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', (__dirname + '/views'));
+app.set('views', ('D:\\home\\site\\wwwroot\\views'));
 app.set('view engine', 'hbs');
 app.engine("hbs", expressHbs(
   {
@@ -34,7 +34,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('D:\\home\\site\\wwwroot\\public'));
 
 app.use('/', indexRouter);
 
